f7f008c796567066708f5bcfdcc4e016f1d399f2
Jacques Favreau <jfavreau@netflix.com>
2016-07-22 20:55:35 -0700

    Add support for nested pragmas.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+'use strict';
 const DISABLE_LINE_REGEX = /^\s*groundskeeper(-willie)?-disable-line\s*$/i;
-const PRAGMA_REGEX = /^\s?<(\/?)([^>*\/]*)>\s*$/;
+const PRAGMA_REGEX = /^\s?<(\/?)([\w\d\-]*)>\s*$/;
 /**
  * Check if a comment is a line-disabling comment.
  */
@@ -10,43 +11,62 @@ const isLineDisableComment = function(comment){
 };
 
 const fetchPragmas = function(comments){
-    let pragmas = {};
+    let completePragmas = [];
+    let openPragmas = [];
     comments.forEach(comment => {
         const pragmaPieces = PRAGMA_REGEX.exec(comment.value);
-        if(pragmaPieces){
+
+        if(pragmaPieces){ // If it's a pragma
             let pragmaName = pragmaPieces[2];
-            let pragmaBookend = pragmaPieces[1] === '/' ? 'end' : 'start';
-            pragmas[pragmaName] = pragmas[pragmaName] || {};
-            pragmas[pragmaName][pragmaBookend] = comment.loc[pragmaBookend];
-            comment.ignore = true;
+            if(pragmaPieces[1] === ''){ // If it's an open:
+                // Push on to the open pragma's list with a name and a loc.
+                openPragmas.push({name: pragmaName, loc: {start: comment.loc.start}});
+            }else { // If it's a close:
+                let found = false;
+                // Run from the back of the list with the name. When found remove from list and push to pragmaOutput
+                for(let i = openPragmas.length - 1; i >= 0; i--){
+                    if(openPragmas[i].name === pragmaName){
+                        found = true;
+                        openPragmas[i].loc.end = comment.loc.end;
+                        completePragmas.push(openPragmas[i]);
+                        openPragmas.splice(i,1);
+                        break;
+                    }
+                }
+                if(!found){ // If you get to the end without closing the pragma, throw an error.
+                    throw new Error(`Error in Groundskeeper Willie: </${pragmaName}> tried to close, but is not open.`);
+                }
+            }
+            comment.ignore = true; // Remove the pragma comment
         }
     });
-    return pragmas;
+    // If there are any left open, throw an error.
+    if(openPragmas.length){
+        throw new Error(`Error in Groundskeeper Willie: Pragma(s) left unclosed ${JSON.stringify(openPragmas, null, 4)}`);
+    }
+    return completePragmas.map(pragma => pragma.loc);
 };
 
 const isInsidePragma = function(node, pragmas){
-    for(let pragmaName in pragmas){ if(pragmas.hasOwnProperty(pragmaName)){
-        const pragma = pragmas[pragmaName];
+    for(let i = 0; i < pragmas.length; i++){
+        const pragma = pragmas[i];
         const nodeLoc = node.loc;
+        if(nodeLoc && nodeLoc.end && nodeLoc.start){
+            const insideStart = nodeLoc.start.line > pragma.start.line ||
+                    (nodeLoc.start.line === pragma.start.line && nodeLoc.start.column > pragma.start.column);
 
-        const insideStart = nodeLoc.start.line > pragma.start.line ||
-                (nodeLoc.start.line === pragma.start.line && nodeLoc.start.column > pragma.start.column);
-        const insideEnd = nodeLoc.end.line < pragma.end.line ||
-                (nodeLoc.end.line === pragma.end.line && nodeLoc.end.column < pragma.end.column);
-        if(insideStart && insideEnd){
-            return true;
+
+            const insideEnd = nodeLoc.end.line < pragma.end.line ||
+                    (nodeLoc.end.line === pragma.end.line && nodeLoc.end.column < pragma.end.column);
+            if(insideStart && insideEnd){
+                return true;
+            }
         }
-    }}
+    }
     return false;
 };
 
-
 const remover = function(path){
-    const isConsole = path.get("callee").matchesPattern("console", true);
-    const isDisabledLine = this.groundskeeperDisabledLines.indexOf(path.node.loc.start.line) !== -1;
-    if (isConsole && !isDisabledLine){
-        path.remove();
-    }
     if(isInsidePragma(path.node, this.groundskeeperPragmas)){
         path.remove();
     }
@@ -57,7 +77,14 @@ const remover = function(path){
  * @type {Object}   The visitor definition
  */
 const removeGroundskeeperElements = {
-    enter: remover
+    enter: remover,
+    CallExpression(path) {
+        const isConsole = path.get("callee").matchesPattern("console", true);
+        const isDisabledLine = this.groundskeeperDisabledLines.indexOf(path.node.loc.start.line) !== -1;
+        if(isConsole && !isDisabledLine){
+            path.remove();
+        }
+    }
 };
 
 module.exports = function(babel) {
---
+++ test/fixtures/remove-nested-pragmas/actual.js
@@ -0,0 +1,11 @@
+module.exports = {
+//<inObjectPragma>
+    prodCode: {
+        crew: ['Leela', 'Fry', 'Bender']
+    } //<inObjectPragma>
+    , testCode: {
+        ships: ['Planet Express', 'Crushinator']
+    }
+    //</inObjectPragma>
+//</inObjectPragma>
+};
